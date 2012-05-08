3cb1f7e238f1cea3452178279604eb3e189b432a
D3L243 <D3L243@Monroe3.pnl.gov>
2012-05-08 16:34:55 -0700

    No longer directly referencing the PHRPReader Visual Studio project; instead referencing DLL in lib folder

--- modMain.vb
+++ modMain.vb
@@ -17,7 +17,7 @@
 
 Module modMain
 
-	Public Const PROGRAM_DATE As String = "April 30, 2012"
+	Public Const PROGRAM_DATE As String = "May 8, 2012"
 
     Private mInputFilePath As String
     Private mOutputFolderPath As String             ' Optional
