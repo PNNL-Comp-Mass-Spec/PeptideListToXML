848027492198c58ddac8300e9b63eefec8c6a29e
alchemistmatt <matthew.monroe@pnnl.gov>
2014-05-05 16:53:58 -0700

    Add clsPSMInfo

---
+++ clsPSMInfo.vb
@@ -0,0 +1,43 @@
+﻿Option Strict On
+
+Public Class clsPSMInfo
+
+	Public Const MSGF_SPEC_NOTDEFINED As Double = 100
+
+	Protected mPSM As PHRPReader.clsPSM
+	Protected mSpectrumKey As String
+	Protected mMSGFSpecProb As Double
+
+	Public ReadOnly Property PSM As PHRPReader.clsPSM
+		Get
+			Return mPSM
+		End Get
+	End Property
+
+	Public ReadOnly Property SpectrumKey As String
+		Get
+			Return mSpectrumKey
+		End Get
+	End Property
+
+	Public ReadOnly Property MSGFSpecProb As Double
+		Get
+			Return mMSGFSpecProb
+		End Get
+	End Property
+
+	Public Sub New(ByVal strSpectrumKey As String, objPSM As PHRPReader.clsPSM)
+		mSpectrumKey = strSpectrumKey
+		mMSGFSpecProb = MSGF_SPEC_NOTDEFINED
+		mPSM = objPSM
+
+		If mPSM Is Nothing Then
+			mMSGFSpecProb = MSGF_SPEC_NOTDEFINED
+		Else
+			If Not Double.TryParse(mPSM.MSGFSpecProb, mMSGFSpecProb) Then
+				mMSGFSpecProb = MSGF_SPEC_NOTDEFINED
+			End If
+		End If
+	End Sub
+
+End Class
