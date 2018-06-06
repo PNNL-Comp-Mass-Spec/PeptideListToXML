ef6a7e06466c6a6e95475584a973b2cc8cfe65e4
alchemistmatt <matthew.monroe@pnnl.gov>
2018-06-06 14:15:12 -0700

    Update PepXML to text scripts to Python 3.x and update to support .pepXML files from MSFragger and MSGF+

--- PepXML_to_Text/pepxml.py
+++ PepXML_to_Text/pepxml.py
@@ -2,6 +2,10 @@
 # Originally from http://code.google.com/p/massspec-toolbox/source/browse/trunk/search/sequest-pepxml2hit_list.py
 # Modified by Matthew Monroe in September 2012 to extract additional columns
 #
+# This library file is used by pepxml2hit_list.py
+#
+# 2018-06-06 mem - Update to Python 3.x and update to support .pepXML files from MSFragger and MSGF+
+#
 
 import xml.sax
 from decimal import Decimal, getcontext
@@ -21,13 +25,19 @@ class pepxml_parser(xml.sax.ContentHandler):
         if( len(self.element_array) == 3 and name == 'spectrum_query' ):
             self.is_spectrum_query = True
             self.spectrum_id = attr['spectrum']
-            if( not self.PSM.has_key(self.spectrum_id) ):
+            if( self.spectrum_id not in self.PSM ):
                 self.PSM[self.spectrum_id] = dict()
                 self.PSM[self.spectrum_id]['search_hit'] = []
             else:
-                print "Duplicate PSM : %s"%self.spectrum_id
+                print("Duplicate PSM : %s"%self.spectrum_id)
+                
             self.PSM[self.spectrum_id]['charge'] = int(attr['assumed_charge'])
             self.PSM[self.spectrum_id]['neutral_mass'] = float(attr['precursor_neutral_mass'])
+
+            self.PSM[self.spectrum_id]['start_scan'] = int(attr['start_scan'])
+            self.PSM[self.spectrum_id]['end_scan'] = int(attr['end_scan'])
+            self.PSM[self.spectrum_id]['retention_time_sec'] = float(attr['retention_time_sec'])
+
         if( len(self.element_array) == 5 and name == 'search_hit' ):
             self.is_search_hit = True
             self.search_hit = dict()
@@ -35,6 +45,10 @@ class pepxml_parser(xml.sax.ContentHandler):
             self.search_hit['peptide'] = attr['peptide']
             self.search_hit['protein'] = attr['protein']
             self.search_hit['missed_cleavages'] = int(attr['num_missed_cleavages'])
+            self.search_hit['NumTrypticEnds'] = int(attr['num_tol_term'])
+            self.search_hit['Ions_Matched'] = int(attr['num_matched_ions'])
+            self.search_hit['Ions_Observed'] = int(attr['tot_num_ions'])
+
         if( len(self.element_array) == 6 and name == 'search_score' ):
             ## SEQUEST
             if(attr['name'] == 'xcorr'):
@@ -43,7 +57,6 @@ class pepxml_parser(xml.sax.ContentHandler):
                 self.search_hit['spscore'] = float(attr['value'])
             if(attr['name'] == 'deltacn'):
                 self.search_hit['deltacn'] = float(attr['value'])
-
             if(attr['name'] == 'deltacnstar'):
                 self.search_hit['deltacnstar'] = float(attr['value'])
             if(attr['name'] == 'RankXc'):
@@ -55,20 +68,23 @@ class pepxml_parser(xml.sax.ContentHandler):
             if(attr['name'] == 'Ions_Expected'):
                 self.search_hit['Ions_Expected'] = int(attr['value'])
 
-            ## X!Tandem
+            ## X!Tandem and MSFragger
             if(attr['name'] == 'hyperscore'):
                 self.search_hit['hyperscore'] = float(attr['value'])
+            if(attr['name'] == 'nextscore'):
+                self.search_hit['nextscore'] = float(attr['value'])
             if(attr['name'] == 'expect'):
                 self.search_hit['expect'] = float(attr['value'])
+                
             ## InsPecT
             if(attr['name'] == 'mqscore'):
                 self.search_hit['mqscore'] = float(attr['value'])
-            if(attr['name'] == 'expect'):
-                self.search_hit['expect'] = float(attr['value'])
+            # InsPecT reports expect values; already handled above
             if(attr['name'] == 'fscore'):
                 self.search_hit['fscore'] = float(attr['value'])
             if(attr['name'] == 'deltascore'):
                 self.search_hit['deltascore'] = float(attr['value'])
+
             ## MyriMatch
             if(attr['name'] == 'mvh'):
                 self.search_hit['mvh'] = float(attr['value'])
@@ -82,19 +98,20 @@ class pepxml_parser(xml.sax.ContentHandler):
                 self.search_hit['newMZFidelity'] = float(attr['value'])
             if(attr['name'] == 'mzMAE'):
                 self.search_hit['mzMAE'] = float(attr['value'])
+
             ## DirecTag-TagRecon
             if(attr['name'] == 'numPTMs'):
                 self.search_hit['numPTMs'] = int(attr['value'])
 
             ## Generic additional
-            if(attr['name'] == 'start_scan'):
-                self.search_hit['start_scan'] = int(attr['value'])
-            if(attr['name'] == 'end_scan'):
-                self.search_hit['end_scan'] = int(attr['value'])
-            if(attr['name'] == 'retention_time_sec'):
-                self.search_hit['retention_time_sec'] = float(attr['value'])
             if(attr['name'] == 'NumTrypticEnds'):
                 self.search_hit['NumTrypticEnds'] = int(attr['value'])
+
+            ## MSGF+
+            # Track MSGF+ EValues using 'expect' aka expectation value
+            if(attr['name'] == 'EValue'):
+                self.search_hit['expect'] = float(attr['value'])
+
             if(attr['name'] == 'msgfspecprob'):
                 if(attr['value'] != ''):
                     self.search_hit['msgfspecprob'] = Decimal(attr['value'])
--- PepXML_to_Text/pepxml2hit_list.py
+++ PepXML_to_Text/pepxml2hit_list.py
@@ -4,25 +4,29 @@
 # Originally from http://code.google.com/p/massspec-toolbox/source/browse/trunk/search/sequest-pepxml2hit_list.py
 # Modified by Matthew Monroe in September 2012 to extract additional columns
 #
+# 2018-06-06 mem - Update to Python 3.x and update to support .pepXML files from MSFragger and MSGF+
+#
 
 import os
 import sys
 import re
+
+# Import pepxml.py, which should be in the same directory as pepxml2hit_list.py
 import pepxml
 
-usage_mesg = 'Usage: pepxml2hit_list.py <.pepxml file>'
+usage_mesg = 'Usage: pepxml2hit_list.py FileToProcess.pepXML'
 
 if( len(sys.argv) != 2 ):
-    print usage_mesg
+    print(usage_mesg)
     sys.exit(1)
 
 filename_pepxml = sys.argv[1]
 if( not os.access(filename_pepxml,os.R_OK) ):
-    print "%s is not accessible."%filename_pepxml
-    print usage_mesg
+    print("%s is not accessible."%filename_pepxml)
+    print(usage_mesg)
     sys.exit(1)
 
-print 'Reading %s'%(filename_pepxml)
+print('Reading %s'%(filename_pepxml))
 
 PSM = pepxml.parse_by_filename(filename_pepxml)
 
@@ -30,14 +34,23 @@ filename_out = filename_pepxml
 filename_out = re.sub('.pepxml$','',filename_out)
 filename_out += '.txt'
 
-print 'Creating %s'%(filename_pepxml)
+print('Creating %s'%(filename_pepxml))
 sys.stderr.write("Write %s ... \n"%filename_out)
 f_out = open(filename_out,'w')
-f_out.write("Spectrum_ID\tCharge\tNeutralMass\tPeptide\tProtein\tMissedCleavages\tXcorr\tDeltaCn\tDeltaCn2\tRankXc\tXcRatio\tIons_Observed\tIons_Expected\tNumTrypticEnds\tMSGF_SpecProb\tScan_Scan\tEnd_Scan\tRetentionTime_Sec\n")
+
+f_out.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % ("Spectrum_ID", "Charge", "NeutralMass", "Peptide", "Protein", "MissedCleavages", "Xcorr", "DeltaCn"))
+f_out.write("\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % ("DeltaCn2", "RankXc", "XcRatio", "Ions_Observed", "Ions_Matched", "Ions_Expected", "NumTrypticEnds", "MSGF_SpecProb", "EValue"))
+f_out.write("\t%s\t%s\t%s\n" % ("Scan_Scan", "End_Scan", "RetentionTime_Sec"))
+
 intLinesWritten = 0
 for spectrum_id in PSM.keys():
     charge = PSM[spectrum_id]['charge']
     neutral_mass = PSM[spectrum_id]['neutral_mass']
+
+    start_scan = PSM[spectrum_id]['start_scan']
+    end_scan = PSM[spectrum_id]['end_scan']
+    retention_time_sec = PSM[spectrum_id]['retention_time_sec']
+
     best_peptide = ''
     best_protein = ''
     best_xcorr = 0
@@ -46,59 +59,70 @@ for spectrum_id in PSM.keys():
     best_RankXc = 0
     best_XcRatio = 0
     best_Ions_Observed = 0
+    best_Ions_Matched = 0
     best_Ions_Expected = 0
     best_NumTrypticEnds = 0
     best_msgfspecprob = 1
+    best_expect = 1
     StoreHit = 0
     HitsParsed = 0
         
     for tmp_hit in PSM[spectrum_id]['search_hit']:
         msgfspecprob = tmp_hit.setdefault('msgfspecprob',1)
+        expectScore = tmp_hit.setdefault('expect',1)
+        xcorr = tmp_hit.setdefault('xcorr',0)
     
         StoreHit = 0
         if (HitsParsed == 0):
             StoreHit = 1
             best_msgfspecprob = msgfspecprob
-            
-            start_scan = tmp_hit.setdefault('start_scan',0)
-            end_scan = tmp_hit.setdefault('end_scan',0)
-            retention_time_sec = tmp_hit.setdefault('retention_time_sec',0)
+            best_expect = expectScore
+            best_xcorr = xcorr
                  
         elif (best_msgfspecprob < 1):
             if (msgfspecprob < best_msgfspecprob):
                 StoreHit = 1
-        elif (tmp_hit['xcorr'] > best_xcorr ):
-            StoreHit = 1
-        
+        elif (best_expect < 1):
+            if (expectScore < best_expect):
+                StoreHit = 1
+        elif (best_xcorr > 0):
+            if (xcorr > best_xcorr):
+                StoreHit = 1
 
         if( StoreHit == 1 ):
-            best_xcorr = tmp_hit['xcorr']
-            best_peptide = tmp_hit['peptide']
-            best_protein = tmp_hit['protein']
-            best_deltacn = tmp_hit['deltacn']
-            missed_cleavages = tmp_hit['missed_cleavages']
-            best_deltacnStar = tmp_hit.setdefault('deltacnstar',0)
-            best_RankXc = tmp_hit.setdefault('RankXc',0)
-            best_XcRatio = tmp_hit.setdefault('XcRatio',0)
-            best_Ions_Observed = tmp_hit.setdefault('Ions_Observed',0)
-            best_Ions_Expected = tmp_hit.setdefault('Ions_Expected',0)
+            best_xcorr          = tmp_hit.setdefault('xcorr',0)
+            best_peptide        = tmp_hit['peptide']
+            best_protein        = tmp_hit['protein']
+            best_deltacn        = tmp_hit.setdefault('deltacn',0)
+            missed_cleavages    = tmp_hit['missed_cleavages']
+            best_deltacnStar    = tmp_hit.setdefault('deltacnstar',0)
+            best_RankXc         = tmp_hit.setdefault('RankXc',0)
+            best_XcRatio        = tmp_hit.setdefault('XcRatio',0)
+            best_Ions_Observed  = tmp_hit.setdefault('Ions_Observed',0)
+            best_Ions_Matched   = tmp_hit.setdefault('Ions_Matched',0)
+            best_Ions_Expected  = tmp_hit.setdefault('Ions_Expected',0)
             best_NumTrypticEnds = tmp_hit.setdefault('NumTrypticEnds',0)
-            best_msgfspecprob = tmp_hit.setdefault('msgfspecprob',1)
+            best_msgfspecprob   = tmp_hit.setdefault('msgfspecprob',1)
+            best_expect         = tmp_hit.setdefault('expect',1)
        
         HitsParsed += 1
-
+    # end for loop over spectrum hits
+    
     if (HitsParsed > 0):
-        f_out.write("%s\t%s\t%f\t%s\t%s\t%d\t%f\t%f"%(spectrum_id, charge, neutral_mass, best_peptide, best_protein, missed_cleavages, best_xcorr, best_deltacn))
+        f_out.write("%s\t%s\t%f\t%s\t%s\t%d\t%f\t%f" % (spectrum_id, charge, neutral_mass, best_peptide, best_protein, missed_cleavages, best_xcorr, best_deltacn))
 
-        f_out.write("\t%f\t%s\t%f\t%s\t%s\t%s\t%s"%(best_deltacnStar, best_RankXc, best_XcRatio, best_Ions_Observed, best_Ions_Expected, best_NumTrypticEnds, best_msgfspecprob))
+        f_out.write("\t%f\t%s\t%f\t%s\t%s\t%s\t%s\t%s\t%s" % (best_deltacnStar, best_RankXc, best_XcRatio, best_Ions_Observed, best_Ions_Matched, best_Ions_Expected, best_NumTrypticEnds, best_msgfspecprob, best_expect))
 
-        f_out.write("\t%s\t%s\t%f\n"%(start_scan, end_scan, retention_time_sec))
+        f_out.write("\t%s\t%s\t%f\n" % (start_scan, end_scan, retention_time_sec))
     
         intLinesWritten += 1
     
-        if (intLinesWritten % 1000 == 0):
-            print '%i / %i'%(intLinesWritten,len(PSM.keys()))
-        
+        if (intLinesWritten % 10000 == 0):
+            print('%i / %i' % (intLinesWritten,len(PSM.keys())))
+    # end if
+
+# end for loop over PSMs
+
 f_out.close()
 
-print "Done"
+print ("Done")
