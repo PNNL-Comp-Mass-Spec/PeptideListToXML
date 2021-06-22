b7ece7b39b436dbe4b54489629055cca71bd82dc
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-21 17:47:55 -0700

    Add batch file

---
+++ ZippedForGitHub/UpdateZipFile.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\7-Zip\7z.exe" a PeptideListToXML.zip ..\bin\PHRPReader.dll ..\bin\PRISM.dll ..\bin\PeptideListToXML.exe ..\bin\PeptideListToXML.exe.config ..\bin\PeptideListToXML.pdb ..\bin\PeptideListToXML.xml ..\Readme.md ..\RevisionHistory.txt
+
+pause
